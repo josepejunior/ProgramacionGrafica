9e2e626e2300d8ec50b09b92a0b1a3816e2ed845
Joseph Mendoza <josephmenjunior@gmail.com>
2023-11-09 09:15:32 -0600

    nose

--- EjemploWinForms/frmMenuPrincipal.Designer.cs
+++ EjemploWinForms/frmMenuPrincipal.Designer.cs
@@ -29,15 +29,17 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pnlBarraTitulo = new System.Windows.Forms.Panel();
-            this.pnlMenu = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.pnlBarraTitulo = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.pnlMenu);
             this.panel1.Controls.Add(this.pnlBarraTitulo);
@@ -47,14 +49,14 @@
             this.panel1.Size = new System.Drawing.Size(1405, 660);
             this.panel1.TabIndex = 0;
             // 
-            // pnlBarraTitulo
+            // panel2
             // 
-            this.pnlBarraTitulo.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.pnlBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlBarraTitulo.Location = new System.Drawing.Point(0, 0);
-            this.pnlBarraTitulo.Name = "pnlBarraTitulo";
-            this.pnlBarraTitulo.Size = new System.Drawing.Size(1405, 53);
-            this.pnlBarraTitulo.TabIndex = 0;
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(307, 560);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1098, 100);
+            this.panel2.TabIndex = 2;
             // 
             // pnlMenu
             // 
@@ -65,14 +67,22 @@
             this.pnlMenu.Size = new System.Drawing.Size(307, 607);
             this.pnlMenu.TabIndex = 1;
             // 
-            // panel2
+            // pnlBarraTitulo
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(307, 560);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1098, 100);
-            this.panel2.TabIndex = 2;
+            this.pnlBarraTitulo.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.pnlBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlBarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlBarraTitulo.Name = "pnlBarraTitulo";
+            this.pnlBarraTitulo.Size = new System.Drawing.Size(1405, 53);
+            this.pnlBarraTitulo.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(307, 53);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1098, 507);
+            this.panel3.TabIndex = 3;
             // 
             // frmMenuPrincipal
             // 
@@ -94,5 +104,6 @@
         private Panel panel2;
         private Panel pnlMenu;
         private Panel pnlBarraTitulo;
+        private Panel panel3;
     }
 }
